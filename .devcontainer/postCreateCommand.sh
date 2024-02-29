2c45010ed184e01ea575239941eefce91aabd23d
Sam Peddamatham <sam.pm@infomagnus.com>
2024-02-29 13:35:00 +0000

    Update

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-npm install -g remark remark-parse remark-stringify
+#npm install -g remark remark-parse remark-stringify
+npm install -g
 
 curl https://sh.rustup.rs -sSf | sh
 cargo install --locked term-transcript-cli --features portable-pty
